6c7516b04559136717a53de21aa81513070a42fd
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-09-26 12:56:39 -0700

    Add files via upload

---
+++ E03.Venegas/T04.Venegas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Datos D = new Datos();
+
+            int op;
+
+            do
+            {
+                try
+                {
+                    D.Captura();
+                    D.Imprimir();
+                }
+
+                catch (FormatException e)
+                {
+                    Console.Write(e.Message);
+                }
+
+                Console.WriteLine("\nQuiere volver a iniciar el programa? \n1=Si \n2=No");
+                op = int.Parse(Console.ReadLine());
+
+            } while (op == 1);
+
+            Console.Clear();
+        }
+    }
+}
---
+++ E03.Venegas/T05.Venegas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TiempoEjecucion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            Stopwatch timeEjecucion = Stopwatch.StartNew();
+
+            for (int i = 1; i <= 1000000; i++) //for para sumar del 1 al 1000000       
+            {
+                sum = sum + i;
+            }
+
+            Console.Write("\nLa suma total es: {0} \nTiempo de ejecucion: {1} ", sum, timeEjecucion.Elapsed);
+
+            Console.ReadKey();
+        }
+    }
+}
+
